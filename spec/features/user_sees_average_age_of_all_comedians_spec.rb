a717cd45c75e9cf015379b6403e58bfb1dcc536c
brickstar <brickstar@gmail.com>
2018-06-29 10:17:02 -0600

    user sees average age spec

---
+++ spec/features/user_sees_average_age_of_all_comedians_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'User' do
+  describe 'visits comedians index' do
+    it 'should see average age of all comedians' do
+      iliza = Comedian.create(name: "Iliza Shlesinger", age: 30)
+      hannibal = Comedian.create(name: "Hannibal Buress", age: 20)
+
+      visit '/comedians'
+
+      within('#average-age') do
+        expect(page).to have_content(25)
+      end
+    end
+  end
+end
